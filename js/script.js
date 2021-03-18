b3ad31393519496207f27e9350fc6fac766f10d8
Lera Sharuk <lera.sharuk@mail.ru>
2021-03-19 02:00:35 +0300

    18 practica functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-//15 практика циклы
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",'');
+//18 практика функции
+let numberOfFilms;
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",'');
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",'');
+    }
+}
+
+start();
+
 
 const peronalMovieDB = {
     count: numberOfFilms,
@@ -18,9 +27,14 @@ if (a != null && b != null && a != '' && b != '' && a.length < 50){
 }
 else {
     console.log('error');
-    const a = prompt("Один из просмотренных фильмов?",''),
-          b = prompt("На сколько оцените его?",'');
 }
 peronalMovieDB.movies[a] = b;
 
 console.log(peronalMovieDB);
+
+function writeYourGenres(){
+    for(let i = 1; i <= 3; i++){
+        peronalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+writeYourGenres();
