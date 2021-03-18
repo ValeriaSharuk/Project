936debb121bec68679f7165d2fc15f2f7d89ebcf
Lera Sharuk <lera.sharuk@mail.ru>
2021-03-18 23:59:04 +0300

    12 practica

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log(1)
+//12 практика
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",'');
+
+const peronalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из просмотренных фильмов?",''),
+      b = prompt("На сколько оцените его?",'');
+
+peronalMovieDB.movies[a] = b;
+
+console.log(peronalMovieDB);
