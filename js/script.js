3c2e5aaf4161ebe4dbe0ccc71d94ea5a32f8b1d3
Lera Sharuk <lera.sharuk@mail.ru>
2021-03-19 00:38:39 +0300

    16 practica cycles

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-//12 практика
+//15 практика циклы
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?",'');
 
 const peronalMovieDB = {
@@ -12,6 +12,15 @@ const peronalMovieDB = {
 const a = prompt("Один из просмотренных фильмов?",''),
       b = prompt("На сколько оцените его?",'');
 
+if (a != null && b != null && a != '' && b != '' && a.length < 50){
+    peronalMovieDB.movies[a] = b;
+    console.log('done');
+}
+else {
+    console.log('error');
+    const a = prompt("Один из просмотренных фильмов?",''),
+          b = prompt("На сколько оцените его?",'');
+}
 peronalMovieDB.movies[a] = b;
 
 console.log(peronalMovieDB);
